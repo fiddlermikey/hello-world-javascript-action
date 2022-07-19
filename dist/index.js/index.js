4448977afe4290feb3eb3fa0a3d6ad29b5cdce65
Michael Henderson <mhenderson@keyfactor.com>
2022-07-19 13:52:34 -0700

    use compiled js

--- dist/index.js/index.js
+++ dist/index.js/index.js
@@ -8866,9 +8866,12 @@ const github = __nccwpck_require__(8408);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const rName = github.context.repo.owner;
+  console.log(`The repo name: ${rName}`);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  core.setOutput("rName", rName);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
