080440131854f66316b4ff63ede1d40e23918699
Michael Henderson <mhenderson@keyfactor.com>
2022-07-19 13:47:21 -0700

    move new log statement

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const rName = await github.context.repo.owner;
+  console.log(`The repo name: ${rName}`);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const rName = await github.context.repo.owner;
-  console.log(`The repo name: ${rName}`);
 } catch (error) {
   core.setFailed(error.message);
 }
