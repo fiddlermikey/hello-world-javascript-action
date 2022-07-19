95949d12029e36287a0d1090bccad3b036962334
Michael Henderson <mhenderson@keyfactor.com>
2022-07-19 14:03:39 -0700

    get branch ref

--- dist/index.js
+++ dist/index.js
@@ -8867,34 +8867,36 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const rName = github.context.repo.repo;
+  const bName = github.context.repo.ref
   console.log(`The repo name: ${rName}`);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   core.setOutput("rName", rName);
+  core.setOutput("bName", bName);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const pulls = github.context.pulls.list({
-    owner: repoOwner,
-    repo: repoName,
-    state: 'closed'
-  });
-  const lastPull = pulls.data[0].id;
-  const lastPullTitle = pulls.data[0].title;
-  const lastPullNum = pulls.data[0].number;
+  //const pulls = github.context.repo.pulls.list({
+  //  owner: repoOwner,
+  //  repo: repoName,
+  //  state: 'closed'
+  //});
+  //const lastPull = pulls.data[0].id;
+  //const lastPullTitle = pulls.data[0].title;
+  //const lastPullNum = pulls.data[0].number;
 
-  const pullCommits = github.context.pulls.listCommits({
-    owner: repoOwner,
-    repo: repoName,
-    pull_number: lastPullNum
-  })
+  //const pullCommits = github.context.pulls.listCommits({
+  //  owner: repoOwner,
+  //  repo: repoName,
+  //  pull_number: lastPullNum
+  //})
 
-  const latestRelease = github.context.repos.getLatestRelease({
-    owner: repoOwner,
-    repo: repoName
+  //const latestRelease = github.context.repos.getLatestRelease({
+  //  owner: repoOwner,
+  //  repo: repoName
 
-  })
+  //})
   
 } catch (error) {
   core.setFailed(error.message);
