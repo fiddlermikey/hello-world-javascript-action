7e3e76ef9232e99c9d14d144354dfc0d775e9a7c
Michael Henderson <mhenderson@keyfactor.com>
2022-07-19 16:39:17 -0700

    add octokit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const repoName = 'githubscripting'
+const repoOwner = 'fiddlermikey'
+
 async function run() {
   console.log("Begin")
   try {
+    const octokit = github.getOctokit(env.GITHUB_TOKEN);
     var commitMessages = ""
   } catch (err) {
     core.setFailed(err.message);
